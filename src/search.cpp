5b625732ad9763f87d0af178211d181ac25d5c3c
MJZ1977 <37274752+MJZ1977@users.noreply.github.com>
2018-04-19 09:24:08 +0200

    Probcut low depth

--- src/search.cpp
+++ src/search.cpp
@@ -781,12 +781,12 @@ namespace {
     // If we have a good enough capture and a reduced search returns a value
     // much above beta, we can (almost) safely prune the previous move.
     if (   !PvNode
-        &&  depth >= 5 * ONE_PLY
+        &&  depth >= 4 * ONE_PLY
         &&  abs(beta) < VALUE_MATE_IN_MAX_PLY)
     {
         assert(is_ok((ss-1)->currentMove));
 
-        Value rbeta = std::min(beta + 152 - 36 * improving, VALUE_INFINITE);
+        Value rbeta = std::min(beta + 212 - 48 * improving - 8 * std::min(depth/ONE_PLY - 7, 6), VALUE_INFINITE);
         MovePicker mp(pos, ttMove, rbeta - ss->staticEval, &thisThread->captureHistory);
         int probCutCount = 0;
 
@@ -799,7 +799,7 @@ namespace {
                 ss->currentMove = move;
                 ss->contHistory = thisThread->contHistory[pos.moved_piece(move)][to_sq(move)].get();
 
-                assert(depth >= 5 * ONE_PLY);
+                assert(depth >= 4 * ONE_PLY);
 
                 pos.do_move(move, st);
 
@@ -808,7 +808,7 @@ namespace {
 
                 // If the qsearch held perform the regular search
                 if (value >= rbeta)
-                    value = -search<NonPV>(pos, ss+1, -rbeta, -rbeta+1, std::max(depth - 3 * ONE_PLY, 4 * ONE_PLY), !cutNode, false);
+                    value = -search<NonPV>(pos, ss+1, -rbeta, -rbeta+1, std::max(depth - 4 * ONE_PLY, 2 * ONE_PLY), !cutNode, false);
 
                 pos.undo_move(move);
 

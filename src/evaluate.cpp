4ce3a8ee2cff95c2b511c22dbd6f96f9163034d8
MJZ1977 <37274752+MJZ1977@users.noreply.github.com>
2018-05-03 10:06:49 +0200

    Bonus if passed pawn + queen

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -653,10 +653,11 @@ namespace {
 
         int r = relative_rank(Us, s);
         int w = PassedDanger[r];
-        
-		if (pos.non_pawn_material(Them) <= BishopValueEg)
+
+		if (pos.non_pawn_material(Them) <= BishopValueMg
+		  || pos.non_pawn_material(Us) == QueenValueMg)
 		  w += 1;
-		
+
         Score bonus = PassedRank[r];
 
         if (w)

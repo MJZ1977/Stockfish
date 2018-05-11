72f9ab9eb14228e79e914064e2aa7e1778123fc6
MJZ1977 <37274752+MJZ1977@users.noreply.github.com>
2018-05-11 13:35:28 +0200

    Small penality for doubled and isolated pawns

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -42,7 +42,7 @@ namespace {
 
   // Doubled pawn penalty
   constexpr Score Doubled = S(10, 30);
-  //constexpr Score Doubled_Isolated = S( 5, 20);
+  constexpr Score Doubled_Isolated = S( 0, 10);
 
   // Strength of pawn shelter for our king by [distance from edge][rank].
   // RANK_1 = 0 is used for files where we have no pawn, or pawn is behind our king.
@@ -150,8 +150,8 @@ namespace {
         if (doubled && !supported)
             score -= Doubled;
 
-		//if (doubled && !neighbours)
-       //     score -= Doubled_Isolated;
+		if (doubled && !neighbours)
+            score -= Doubled_Isolated;
     }
 
     return score;

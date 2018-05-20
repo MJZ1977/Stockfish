ea46520fbdecfa100baa4e8156508e2b5b0a737d
MJZ1977 <37274752+MJZ1977@users.noreply.github.com>
2018-05-21 01:12:35 +0200

    No bishop, limit to 2 squares

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -181,7 +181,7 @@ namespace {
   constexpr Score TrappedRook        = S( 92,  0);
   constexpr Score WeakQueen          = S( 50, 10);
   constexpr Score WeakUnopposedPawn  = S(  5, 25);
-  constexpr Score EnemyWeakness      = S(  8,  0);
+  constexpr Score EnemyWeakness      = S( 24,  0);
 
 #undef S
 
@@ -623,9 +623,9 @@ namespace {
 
 	// Safe squares in enemy camp
 	b = EnemyCamp & ~attackedBy[Them][ALL_PIECES];
-	b &= (pos.pieces(Us) | attackedBy[Us][KNIGHT] | attackedBy[Us][BISHOP] | attackedBy[Us][ROOK] | attackedBy[Us][QUEEN]);
+	b &= (pos.pieces(Us) | attackedBy[Us][KNIGHT] | attackedBy[Us][ROOK] | attackedBy[Us][QUEEN]);
 	
-	score += EnemyWeakness * popcount(b);
+	score += EnemyWeakness * std::min(popcount(b),2);
 	
     if (T)
         Trace::add(THREAT, Us, score);

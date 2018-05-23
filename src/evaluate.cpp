801c906961a35ca747ccebd7e4e115558c157207
MJZ1977 <37274752+MJZ1977@users.noreply.github.com>
2018-05-23 11:09:51 +0200

    Compensation

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -812,12 +812,12 @@ namespace {
             // Endgame with opposite-colored bishops and no other pieces is almost a draw
             if (   pos.non_pawn_material(WHITE) == BishopValueMg
                 && pos.non_pawn_material(BLACK) == BishopValueMg)
-                sf = 31;
+                sf = 34;
 
             // Endgame with opposite-colored bishops, but also other pieces. Still
             // a bit drawish, but not as drawish as with only the two bishops.
             else
-                sf = 46;
+                sf = 48;
         }
         else
             sf = std::min(40 + 7 * pos.count<PAWN>(strongSide), sf);

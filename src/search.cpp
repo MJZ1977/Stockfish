4e08007e451a2d13b41e53f57d58ba7d6cabaa41
MJZ1977 <37274752+MJZ1977@users.noreply.github.com>
2018-05-30 00:01:04 +0200

    Improving ss-4

--- src/search.cpp
+++ src/search.cpp
@@ -735,6 +735,10 @@ namespace {
 
     improving =   ss->staticEval >= (ss-2)->staticEval
                || (ss-2)->staticEval == VALUE_NONE;
+			   
+	if (ss->ply > 4)
+	    improving = improving || ss->staticEval >= (ss-4)->staticEval
+               || (ss-4)->staticEval == VALUE_NONE;
 
     // Step 8. Futility pruning: child node (~30 Elo)
     if (   !rootNode

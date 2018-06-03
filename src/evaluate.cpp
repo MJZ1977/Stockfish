01f97bcde8ced0b657e4a81ce948b25e45f3ae0d
MJZ1977 <37274752+MJZ1977@users.noreply.github.com>
2018-06-03 11:46:17 +0200

    If no rook left, more bonus

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -652,7 +652,10 @@ namespace {
 
         int r = relative_rank(Us, s);
         int w = PassedDanger[r];
-
+        
+		if (pos.count<ROOK>(Them) == 0)
+            w += 1;
+			
         Score bonus = PassedRank[r];
 
         if (w)

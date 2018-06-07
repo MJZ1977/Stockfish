d4002a91c6ff75d8f3a2c615ce7196218837d21f
MJZ1977 <37274752+MJZ1977@users.noreply.github.com>
2018-06-07 03:06:59 +0200

    Tuning

--- src/search.cpp
+++ src/search.cpp
@@ -491,9 +491,9 @@ void Thread::search() {
              weak_second = false;
 			 if (rootDepth >= 8 * ONE_PLY)
              {
-                 Value ralpha = std::max(bestValue - Value(180), -VALUE_MATE);
+                 Value ralpha = std::max(bestValue - Value(160), -VALUE_MATE);
                  ss->excludedMove = lastBestMove;
-                 secondValue = ::search<NonPV>(rootPos, ss, ralpha, ralpha+1, rootDepth - 2*ONE_PLY, true);
+                 secondValue = ::search<NonPV>(rootPos, ss, ralpha, ralpha+1, rootDepth - 2*ONE_PLY, false);
                  ss->excludedMove = MOVE_NONE;
                  if (secondValue <= ralpha)
                     weak_second = true;
@@ -501,8 +501,8 @@ void Thread::search() {
 
               // Stop the search if we have only one legal move, or if available time elapsed
               if (   rootMoves.size() == 1
-                  || Time.elapsed() > Time.optimum() * bestMoveInstability * improvingFactor / 581 
-				     / (weak_second? 2 : 1) )
+                  || Time.elapsed() > Time.optimum() * bestMoveInstability * improvingFactor 
+				     / (weak_second? 1024 : 581) )
               {
                   // If we are allowed to ponder do not stop the search now but
                   // keep pondering until the GUI sends "ponderhit" or "stop".

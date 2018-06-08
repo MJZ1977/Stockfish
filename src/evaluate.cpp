683dafd48b2e5c8b23c366d84f94d777fea4f5bf
MJZ1977 <37274752+MJZ1977@users.noreply.github.com>
2018-06-08 12:45:15 +0200

    RookOpenPos = (16, 0)

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -174,7 +174,7 @@ namespace {
   constexpr Score Overload           = S( 10,  5);
   constexpr Score PawnlessFlank      = S( 20, 80);
   constexpr Score RookOnPawn         = S(  8, 24);
-  constexpr Score RookOpenPos        = S( 10,  4);
+  constexpr Score RookOpenPos        = S( 16,  0);
   constexpr Score SliderOnQueen      = S( 42, 21);
   constexpr Score ThreatByPawnPush   = S( 47, 26);
   constexpr Score ThreatByRank       = S( 16,  3);

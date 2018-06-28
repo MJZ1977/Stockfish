e921c88cacfcf57062d82f30496eeaeb09dabc96
MJZ1977 <37274752+MJZ1977@users.noreply.github.com>
2018-06-28 15:58:47 +0200

    only bad moves, margin 300

--- src/search.cpp
+++ src/search.cpp
@@ -915,7 +915,7 @@ moves_loop: // When in check, search starts from here
       else if (pos.non_pawn_material() == 0
       			&&  abs(ss->staticEval) <= Value(160)
       			&&  abs(ss->staticEval) >= Value(5)
-      			&&  (ss->staticEval >= alpha - Value(400) && ss->staticEval <= beta + Value(400))
+      			&&  !(ss->staticEval >= alpha - Value(300) && ss->staticEval <= beta + Value(300))
       			&&  pos.rule50_count() <= 10
       			&&  depth >= 4 * ONE_PLY
 				&&  (PvNode || improving))	// Endgame extension

81afa2c60aea0a5a57bd2481068ab7cc5336d1bb
MJZ1977 <37274752+MJZ1977@users.noreply.github.com>
2018-07-04 19:15:21 +0200

    (12,0)

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -181,7 +181,7 @@ namespace {
   constexpr Score TrappedRook        = S( 92,  0);
   constexpr Score WeakQueen          = S( 50, 10);
   constexpr Score WeakUnopposedPawn  = S(  5, 26);
-  constexpr Score EnemyWeakness      = S(  8,  0);
+  constexpr Score EnemyWeakness      = S( 12,  0);
 
 #undef S
 

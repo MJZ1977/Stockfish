eac4dbf4946e8f619f49f3fde4fde9a106237b56
MJZ1977 <37274752+MJZ1977@users.noreply.github.com>
2018-07-11 15:45:31 +0200

    Copy from Stefano80 work

--- src/search.cpp
+++ src/search.cpp
@@ -444,6 +444,9 @@ void Thread::search() {
          lastBestMoveDepth = rootDepth;
       }
 
+      if (mainThread)
+        playout(lastBestMove, ss);
+
       // Have we found a "mate in x"?
       if (   Limits.mate
           && bestValue >= VALUE_MATE_IN_MAX_PLY
@@ -502,6 +505,20 @@ void Thread::search() {
                 skill.best ? skill.best : skill.pick_best(multiPV)));
 }
 
+void Thread::playout(Move playMove, Stack* ss) {
+    StateInfo st;
+    bool ttHit;
+    rootPos.do_move(playMove, st);
+    TTEntry* tte    = TT.probe(rootPos.key(), ttHit);
+    Value ttValue   = ttHit ? value_from_tt(tte->value(), ss->ply) : VALUE_NONE;
+    Move ttMove     = ttHit ? tte->move() : MOVE_NONE;
+    if(ttHit && ttMove != MOVE_NONE && MoveList<LEGAL>(rootPos).size() && ss->ply < MAX_PLY){
+        (ss+1)->ply = ss->ply + 1;
+        qsearch<NonPV>(rootPos, ss+1, ttValue-1, ttValue, DEPTH_ZERO);
+        playout(ttMove, ss+1);
+    }
+    rootPos.undo_move(playMove);
+}
 
 namespace {
 

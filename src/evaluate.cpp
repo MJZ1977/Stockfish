5333b1137f596217e07f0c0252226036b8a625c0
MJZ1977 <37274752+MJZ1977@users.noreply.github.com>
2018-08-17 00:26:15 +0200

    passed bonus 1/32

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -699,10 +699,8 @@ namespace {
             || (pos.pieces(PAWN) & forward_file_bb(Us, s)))
             bonus = bonus / 2;
 
-        //if (pos.side_to_move()==Us)
-        //   bonus = bonus * 17 / 16;
-		//else
-		//   bonus = bonus * 15 / 16;
+        if (pos.side_to_move()==Us)
+           bonus = bonus * 33 / 32;
 
         score += bonus + PassedFile[file_of(s)];
     }

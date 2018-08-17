385d5f55f1628bf1995d39e7261b68485eb382cc
MJZ1977 <37274752+MJZ1977@users.noreply.github.com>
2018-08-17 12:19:54 +0200

    kingdanger 58xStM

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -481,7 +481,7 @@ namespace {
                      +   4 * tropism
                      - 873 * !pos.count<QUEEN>(Them)
                      -   6 * mg_value(score) / 8
-                     +  42 * (pos.side_to_move()==Us ? -1 : 1)
+                     +  58 * (pos.side_to_move()==Us ? -1 : 1)
                      -   30;
 
         // Transform the kingDanger units into a Score, and subtract it from the evaluation

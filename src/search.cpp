1b8dc9fe4cfbf0d8eb01a6187d24dab9e3b2c945
MJZ1977 <37274752+MJZ1977@users.noreply.github.com>
2018-08-19 19:18:20 +0200

    First try, depth > 8, gap > 600

--- src/search.cpp
+++ src/search.cpp
@@ -923,6 +923,11 @@ moves_loop: // When in check, search starts from here
                && !moveCountPruning
                &&  pos.see_ge(move))
           extension = ONE_PLY;
+	  else if (   depth >= 8 * ONE_PLY &&
+	           (ss-2)->staticEval != VALUE_NONE &&
+	            ss->staticEval != VALUE_NONE &&
+	           (abs(ss->staticEval - (ss-2)->staticEval) >= Value(600)))
+		  extension = ONE_PLY;
 
       // Calculate new depth for this move
       newDepth = depth - ONE_PLY + extension;

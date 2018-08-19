ed6818285255ad0686ef4b788dd6b6c898297841
MJZ1977 <37274752+MJZ1977@users.noreply.github.com>
2018-08-19 23:43:13 +0200

    Add threat if pawn is protected by another pawn

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -580,7 +580,7 @@ namespace {
     b |= shift<Up>(b & TRank3BB) & ~pos.pieces();
 
     // Keep only the squares which are relatively safe
-    b &= ~attackedBy[Them][PAWN] & safe;
+    b &= (~attackedBy[Them][PAWN] | attackedBy[Us][PAWN]) & safe;
 
     // Bonus for safe pawn threats on the next move
     b = pawn_attacks_bb<Us>(b) & pos.pieces(Them);

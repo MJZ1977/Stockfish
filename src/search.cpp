b858842af5e71e4b02ebbb26ca1144714dade68f
MJZ1977 <37274752+MJZ1977@users.noreply.github.com>
2018-09-20 13:10:06 +0200

    eval <= 160

--- src/search.cpp
+++ src/search.cpp
@@ -997,8 +997,7 @@ moves_loop: // When in check, search starts from here
 
       if (pos.non_pawn_material() == 0 && last_nonPawnMaterial > 0
           && extension == DEPTH_ZERO
-          && abs(eval) <= Value(180)
-          && abs(eval) >= Value(20))
+          && abs(eval) <= Value(160))
          newDepth += ONE_PLY;
 
       // Step 16. Reduced depth search (LMR). If the move fails high it will be

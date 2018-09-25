cc13e8efa0b57a0c5b7ba874745d1dd4b2fa739f
MJZ1977 <37274752+MJZ1977@users.noreply.github.com>
2018-09-25 09:41:42 +0200

    Progressive 0..4

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -803,8 +803,9 @@ namespace {
             while (b)
             {
                Square s = pop_lsb(&b) + (strongSide == WHITE ? NORTH : SOUTH);
-               if (distance(pos.square<KING>(strongSide), s) < distance(pos.square<KING>(~strongSide), s) - 1)
-                 sf += 3;
+               //if (distance(pos.square<KING>(strongSide), s) < distance(pos.square<KING>(~strongSide), s) - 1)
+               sf += std::min(4,std::max(0,
+                    (distance(pos.square<KING>(~strongSide), s) - distance(pos.square<KING>(strongSide), s) + 1)));
 		    }
 		}
         else

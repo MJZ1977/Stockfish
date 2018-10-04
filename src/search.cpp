8d85341a3103962fb7a2bb73b39abfa508e6816b
MJZ1977 <37274752+MJZ1977@users.noreply.github.com>
2018-10-04 13:31:52 +0200

    limit alpha - 80

--- src/search.cpp
+++ src/search.cpp
@@ -899,7 +899,7 @@ moves_loop: // When in check, search starts from here
 
       moveCountPruning =   depth < 16 * ONE_PLY
                         && moveCount >= FutilityMoveCounts[improving][depth / ONE_PLY]
-                                        + (eval < alpha - Value(100)? 1 : 0);
+                                        + (eval < alpha - Value(80)? 1 : 0);
 
       // Step 13. Extensions (~70 Elo)
 

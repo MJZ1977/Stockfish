fb007736662099f96f272397a59ae24b94b30b4b
MJZ1977 <37274752+MJZ1977@users.noreply.github.com>
2018-10-05 18:28:53 +0200

    probcutcount < 4

--- src/search.cpp
+++ src/search.cpp
@@ -813,7 +813,7 @@ namespace {
         int probCutCount = 0;
 
         while (  (move = mp.next_move()) != MOVE_NONE
-               && probCutCount < 3)
+               && probCutCount < 4)
             if (move != excludedMove && pos.legal(move))
             {
                 probCutCount++;

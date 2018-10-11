231f0773f592463fb5da71c2beb0a05ca9b43c7e
MJZ1977 <37274752+MJZ1977@users.noreply.github.com>
2018-10-11 10:54:02 +0200

    rbeta = 224

--- src/search.cpp
+++ src/search.cpp
@@ -808,12 +808,12 @@ namespace {
         &&  depth >= 5 * ONE_PLY
         &&  abs(beta) < VALUE_MATE_IN_MAX_PLY)
     {
-        Value rbeta = std::min(beta + 216 - 48 * improving, VALUE_INFINITE);
+        Value rbeta = std::min(beta + 224 - 48 * improving, VALUE_INFINITE);
         MovePicker mp(pos, ttMove, rbeta - ss->staticEval, &thisThread->captureHistory);
         int probCutCount = 0;
 
         while (  (move = mp.next_move()) != MOVE_NONE
-               && probCutCount < 4)
+               && probCutCount < 3)
             if (move != excludedMove && pos.legal(move))
             {
                 probCutCount++;

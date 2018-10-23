b8cd27dcac9a2f50e2effd736056b22a2ccbf5d0
MJZ1977 <37274752+MJZ1977@users.noreply.github.com>
2018-10-23 16:08:36 +0200

    First try (0,20)

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -160,6 +160,7 @@ namespace {
   constexpr Score CorneredBishop     = S( 50, 50);
   constexpr Score Hanging            = S( 57, 32);
   constexpr Score KingProtector      = S(  6,  6);
+  constexpr Score KingBlocked        = S(  0, 20);
   constexpr Score KnightOnQueen      = S( 21, 11);
   constexpr Score LongDiagonalBishop = S( 46,  0);
   constexpr Score MinorBehindPawn    = S( 16,  0);
@@ -497,6 +498,20 @@ namespace {
 
     // King tropism bonus, to anticipate slow motion attacks on our king
     score -= CloseEnemies * tropism;
+
+    // In endgame : verify that king is not blocked along a row or a file
+    if (pos.non_pawn_material() < EndgameLimit * 2)
+    {
+       b = attackedBy[Us][KING] & ~(attackedBy[Them][ALL_PIECES] | pos.pieces(Us));
+       if (!(b & (file_bb(ksq) << 1)) && (file_of(ksq) < FILE_F))
+          score -= KingBlocked;
+       if (!(b & (file_bb(ksq) >> 1)) && (file_of(ksq) > FILE_C))
+          score -= KingBlocked;
+       if (!(b & (rank_bb(ksq) << 8)) && (rank_of(ksq) < RANK_6))
+          score -= KingBlocked;
+       if (!(b & (rank_bb(ksq) >> 8)) && (rank_of(ksq) > RANK_3))
+          score -= KingBlocked;
+    }
 
     if (T)
         Trace::add(KING, Us, score);

1e863bbcc2df92f16fbe181ce1580bef6e390d79
Moez Jellouli <37274752+MJZ1977@users.noreply.github.com>
2018-10-28 14:11:04 +0100

    Save

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -165,7 +165,7 @@ namespace {
   constexpr Score MinorBehindPawn    = S( 16,  0);
   constexpr Score Overload           = S( 13,  6);
   constexpr Score PawnlessFlank      = S( 19, 84);
-  constexpr Score QueenPin           = S( 40,  8);
+  constexpr Score QueenPin           = S( 50, 10);
   constexpr Score QueenDiscovered    = S( 60, 12);
   constexpr Score RookOnPawn         = S( 10, 30);
   constexpr Score SliderOnQueen      = S( 42, 21);

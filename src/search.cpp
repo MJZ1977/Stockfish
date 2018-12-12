a1201794c6f537673d70bf067314b64a27be17bc
MJZ1977 <37274752+MJZ1977@users.noreply.github.com>
2018-12-12 12:40:42 +0100

    use futility_margin

--- src/search.cpp
+++ src/search.cpp
@@ -764,7 +764,7 @@ namespace {
                || (ss-2)->staticEval == VALUE_NONE;
 
     AccurateLowerBound = ttHit
-                && tte->depth() >= std::max(depth - 2 * ONE_PLY, 6 * ONE_PLY)
+                && tte->depth() >= std::max(depth - 4 * ONE_PLY, 8 * ONE_PLY)
                 && (tte->bound() & BOUND_LOWER)
                 && eval == ttValue;
 
@@ -776,7 +776,8 @@ namespace {
         return eval;
 
     if (AccurateLowerBound
-       &&  eval >= beta + Value(360)
+       &&  !PvNode
+       &&  eval >= beta - futility_margin(depth - tte->depth(), improving)
        &&  eval < VALUE_KNOWN_WIN)
        return eval;
 

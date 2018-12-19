4313f818f6735d72f5940dc60e2f491085c16373
MJZ1977 <37274752+MJZ1977@users.noreply.github.com>
2018-12-19 14:24:58 +0100

    (2,1) x (mob - 6)

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -354,7 +354,7 @@ namespace {
                 // Bonus for bishop controlling several squares with no opposed bishop
                 if (!(((DarkSquares & s)? DarkSquares : ~DarkSquares) & pos.pieces(Them,BISHOP))
                    && pos.count<PAWN>() > 7)
-                    score += make_score(2,0) * (mob - 4);
+                    score += make_score(2,1) * (mob - 6);
             }
 
             // An important Chess960 pattern: A cornered bishop blocked by a friendly

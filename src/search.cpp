96b960e38696e5602ae36bc173f6e10006eef320
MJZ1977 <37274752+MJZ1977@users.noreply.github.com>
2018-12-27 16:32:37 +0100

    remove abs

--- src/search.cpp
+++ src/search.cpp
@@ -1028,7 +1028,7 @@ moves_loop: // When in check, search starts from here
           Depth r = reduction<PvNode>(improving, depth, moveCount);
 
           if (pos.non_pawn_material() < 2 * RookValueMg
-              && abs(eval) >= Value(400)
+              && eval >= Value(400)
               && !PvNode)
               r += ONE_PLY;
 

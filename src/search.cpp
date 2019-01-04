73e6efaabc32e8f765fc474ad1a0bf6fb2a694fe
MJZ1977 <37274752+MJZ1977@users.noreply.github.com>
2019-01-04 12:20:19 +0100

    reduce by 2

--- src/search.cpp
+++ src/search.cpp
@@ -1045,7 +1045,7 @@ moves_loop: // When in check, search starts from here
           Depth r = reduction<PvNode>(improving, depth, moveCount);
 
           if (pvHit && !PvNode)
-              r -= ONE_PLY;
+              r -= 2 * ONE_PLY;
 
           // Decrease reduction if opponent's move count is high (~10 Elo)
           if ((ss-1)->moveCount > 15)

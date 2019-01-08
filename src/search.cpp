6f9e202566a13653ff81727ada1afb962acae2ac
Moez Jellouli <37274752+MJZ1977@users.noreply.github.com>
2019-01-08 22:12:51 +0100

    depth > 5 instead of 6

--- src/search.cpp
+++ src/search.cpp
@@ -677,7 +677,7 @@ namespace {
         return ttValue;
     }
 
-	if (depth > 6 * ONE_PLY && !excludedMove && PvNode)
+	if (depth > 5 * ONE_PLY && !excludedMove && PvNode)
            pvHit = true;
 
     // Step 5. Tablebases probe

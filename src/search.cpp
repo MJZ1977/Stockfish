6d244b9f7c9fd9383a90953a52bd83993862ee02
MJZ1977 <37274752+MJZ1977@users.noreply.github.com>
2019-02-05 10:04:31 +0100

    r+ = alpha / 360

--- src/search.cpp
+++ src/search.cpp
@@ -1032,7 +1032,7 @@ moves_loop: // When in check, search starts from here
           Depth r = reduction<PvNode>(improving, depth, moveCount);
 
           // Winning side : concentrate on verifying winning line, losing side : try other lines
-          r += int(alpha / 400) * ONE_PLY;
+          r += int(alpha / 360) * ONE_PLY;
 
           // Decrease reduction if position is or has been on the PV
           if (pvHit)

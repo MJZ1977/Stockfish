8cb5eeef994f8749b47093e29c0024f51a364dc0
Moez Jellouli <37274752+MJZ1977@users.noreply.github.com>
2019-02-09 18:22:31 +0100

    0.95 + (stab.depth / 10)^2

--- src/search.cpp
+++ src/search.cpp
@@ -498,7 +498,7 @@ void Thread::search() {
           fallingEval        = std::max(0.5, std::min(1.5, fallingEval));
 
           // If the bestMove is stable over several iterations, reduce time accordingly
-          timeReduction = 1.0 + pow(double((completedDepth - lastBestMoveDepth) / ONE_PLY) / 10, 0.5);
+          timeReduction = 0.95 + pow(double((completedDepth - lastBestMoveDepth) / ONE_PLY) / 10, 2);
 
           // Use part of the gained time from a previous stable move for the current move
           double bestMoveInstability = 1.0 + mainThread->bestMoveChanges;

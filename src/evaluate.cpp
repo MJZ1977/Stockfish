ad72cb034c62cef0dd7a18ba7ca31ffe7ae224bd
Moez Jellouli <37274752+MJZ1977@users.noreply.github.com>
2019-02-13 22:14:05 +0100

    Correction

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -292,7 +292,8 @@ namespace {
         // Find attacked squares, including x-ray attacks for bishops and rooks
         b = Pt == BISHOP ? attacks_bb<BISHOP>(s, pos.pieces() ^ pos.pieces(QUEEN))
           : Pt ==   ROOK ? attacks_bb<  ROOK>(s, pos.pieces() ^ pos.pieces(QUEEN) ^ pos.pieces(Us, ROOK))
-		  : Pt ==  QUEEN ? attacks_bb< QUEEN>(s, pos.pieces() ^ pos.pieces(Us, ROOK) ^ pos.pieces(Us, BISHOP))
+		  : Pt ==  QUEEN ? attacks_bb<BISHOP>(s, pos.pieces() ^ pos.pieces(Us, BISHOP)) |
+		                   attacks_bb<  ROOK>(s, pos.pieces() ^ pos.pieces(Us, ROOK))
                          : pos.attacks_from<Pt>(s);
 
         if (pos.blockers_for_king(Us) & s)

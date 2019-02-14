759595a07c30bd30c5de55af2461e9619a08bce2
MJZ1977 <37274752+MJZ1977@users.noreply.github.com>
2019-02-14 11:31:37 +0100

    + 1/4

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -686,7 +686,7 @@ namespace {
                ((attackedBy2[Us] & ~attackedBy2[Them])
                | (attackedBy[Us][ALL_PIECES] & ~attackedBy[Them][ALL_PIECES])
                |  attackedBy[Us][PAWN]) & s)
-               bonus += bonus / 16 ;
+               bonus += bonus / 4 ;
 
         } // rank > RANK_3
 

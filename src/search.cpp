0f9cddfd14bbdf36ea9e5abe597ed70d0cfc6fda
Moez Jellouli <37274752+MJZ1977@users.noreply.github.com>
2019-02-15 22:05:37 +0100

    0.63 >> 0.65

--- src/search.cpp
+++ src/search.cpp
@@ -160,7 +160,7 @@ void Search::init() {
       for (int d = 1; d < 64; ++d)
           for (int mc = 1; mc < 64; ++mc)
           {
-              double r = (1 - exp(-double(d)/5)) * log(mc) / 0.63;
+              double r = (1 - exp(-double(d)/5)) * log(mc) / 0.65;
 
               Reductions[NonPV][imp][d][mc] = int(std::round(r));
               Reductions[PV][imp][d][mc] = std::max(Reductions[NonPV][imp][d][mc] - 1, 0);

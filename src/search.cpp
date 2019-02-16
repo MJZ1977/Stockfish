76e63d521f63f95cdf01e5a52d2b509eb1021cfb
Moez Jellouli <37274752+MJZ1977@users.noreply.github.com>
2019-02-16 18:34:58 +0100

    First try NPM > 8000

--- src/search.cpp
+++ src/search.cpp
@@ -1044,6 +1044,10 @@ moves_loop: // When in check, search starts from here
               // Increase reduction for cut nodes (~5 Elo)
               if (cutNode)
                   r += 2 * ONE_PLY;
+			  
+			  // Increase reduction for king moves at MG
+			  if (type_of(movedPiece) == KING && pos.non_pawn_material() > 8000 && type_of(move) != CASTLING)
+				  r += ONE_PLY;
 
               // Decrease reduction for moves that escape a capture. Filter out
               // castling moves, because they are coded as "king captures rook" and

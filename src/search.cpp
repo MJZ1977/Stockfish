683ac070492bb4605049ae0541ba1d372c774acb
MJZ1977 <37274752+MJZ1977@users.noreply.github.com>
2019-02-25 15:12:48 +0100

    r += 1 + !ttPv

--- src/search.cpp
+++ src/search.cpp
@@ -1039,8 +1039,8 @@ moves_loop: // When in check, search starts from here
                   r += ONE_PLY;
 
               // Increase reduction for cut nodes (~5 Elo)
-              if (cutNode && !ttPv)
-                  r += 2 * ONE_PLY;
+              if (cutNode)
+                  r += (1 + !ttPv) * ONE_PLY;
 
               // Decrease reduction for moves that escape a capture. Filter out
               // castling moves, because they are coded as "king captures rook" and

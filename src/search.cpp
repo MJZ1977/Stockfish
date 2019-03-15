abc7ba0b55eae18bae7ea257c7eb06db6614294d
Moez Jellouli <37274752+MJZ1977@users.noreply.github.com>
2019-03-15 21:34:23 +0100

    cleaning

--- src/search.cpp
+++ src/search.cpp
@@ -622,17 +622,15 @@ namespace {
             : ttHit    ? tte->move() : MOVE_NONE;
     ttPv = (ttHit && tte->is_pv()) || (PvNode && depth > 4 * ONE_PLY);
 
-	// if position has been searched at higher depths and we are shuffling, return value_draw
-	if (pos.rule50_count() > 18 
-	    && ss->ply > 18 
-		&& depth < 3 * ONE_PLY
-        && ttHit		
-		&& tte->depth() > depth
-		&& tte->depth() <= depth + ss->ply * ONE_PLY		//try to avoid searchs at previous moves
-		//&& ttValue < VALUE_DRAW
-		&& pos.count<PAWN>() > 0)
-		     return VALUE_DRAW;
-             //return ttValue * std::max(40 - std::min(pos.rule50_count(), ss->ply), 0) / 40;
+    // if position has been searched at higher depths and we are shuffling, return value_draw
+    if (pos.rule50_count() > 18 
+        && ss->ply > 18 
+        && depth < 3 * ONE_PLY
+        && ttHit        
+        && tte->depth() > depth
+        && tte->depth() <= depth + ss->ply * ONE_PLY        //try to avoid searchs at previous moves
+        && pos.count<PAWN>() > 0)
+             return VALUE_DRAW;
 
     // At non-PV nodes we check for an early TT cutoff
     if (  !PvNode

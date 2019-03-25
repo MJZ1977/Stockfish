82a7758286b7e18eb28356281be203e49bf27c5c
MJZ1977 <37274752+MJZ1977@users.noreply.github.com>
2019-03-25 17:27:50 +0100

    Only critical moves

--- src/search.cpp
+++ src/search.cpp
@@ -937,7 +937,8 @@ moves_loop: // When in check, search starts from here
           extension = ONE_PLY;
 
       // Unsafe positions extension
-      else if (unsafe && depth < 10 * ONE_PLY && !rootNode && ttPv)
+      else if (unsafe && depth < 8 * ONE_PLY && !rootNode && ttPv
+               && (move == ttMove || givesCheck || captureOrPromotion))
           extension = ONE_PLY;
 
       // Calculate new depth for this move

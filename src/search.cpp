6c317b12dc4a28be905d32951e85cde5b4a9946c
Moez Jellouli <37274752+MJZ1977@users.noreply.github.com>
2019-03-27 21:24:21 +0100

    decrease reduction

--- src/search.cpp
+++ src/search.cpp
@@ -1009,7 +1009,7 @@ moves_loop: // When in check, search starts from here
               r -= ONE_PLY;
 		  
 		  else if (kingSafety)
-			  r += ONE_PLY;
+			  r -= ONE_PLY;
 
           // Decrease reduction if opponent's move count is high (~10 Elo)
           if ((ss-1)->moveCount > 15)

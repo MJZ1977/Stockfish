9f1cb4a0d9b71199fbcdd148c5fe6b19dba21e56
MJZ1977 <37274752+MJZ1977@users.noreply.github.com>
2019-04-05 12:24:59 +0200

    correction

--- src/search.cpp
+++ src/search.cpp
@@ -1016,7 +1016,7 @@ moves_loop: // When in check, search starts from here
 	      }
 
           // Decrease reduction if position is or has been on the PV
-          else if (ttPv)
+          else if (ttPv && !rootNode)
               r -= ONE_PLY;
 
           // Decrease reduction if opponent's move count is high (~10 Elo)

b1ba0d1d9af873d46228d466329457eadffd018c
Moez Jellouli <37274752+MJZ1977@users.noreply.github.com>
2019-04-28 13:35:50 +0200

    first try, beta - delta

--- src/search.cpp
+++ src/search.cpp
@@ -374,6 +374,9 @@ void Thread::search() {
           while (true)
           {
               Depth adjustedDepth = std::max(ONE_PLY, rootDepth - failedHighCnt * ONE_PLY);
+			  /*sync_cout << "alpha = " << alpha
+			            << " beta = " << beta
+			            << " delta = " << delta << sync_endl;*/
               bestValue = ::search<PV>(rootPos, ss, alpha, beta, adjustedDepth, false);
 
               // Bring the best move to the front. It is critical that sorting
@@ -402,7 +405,7 @@ void Thread::search() {
               // re-search, otherwise exit the loop.
               if (bestValue <= alpha)
               {
-                  beta = (alpha + beta) / 2;
+                  beta = std::max((alpha + beta) / 2, beta - delta);
                   alpha = std::max(bestValue - delta, -VALUE_INFINITE);
 
                   if (mainThread)

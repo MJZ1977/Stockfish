bcdd743693ad81e176f725ec268879c47b5ddc8e
MJZ1977 <37274752+MJZ1977@users.noreply.github.com>
2019-04-29 12:03:16 +0200

    complex + 50

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -763,7 +763,7 @@ namespace {
                     +  9 * outflanking
                     + 18 * pawnsOnBothFlanks
                     + 49 * !pos.non_pawn_material()
-                    + 100 * steadyAdvantage
+                    + 50 * steadyAdvantage
                     -103 ;
 
     // Now apply the bonus: note that we find the attacking side by extracting

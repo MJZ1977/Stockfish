3bd1a78ece2e33320054c702df160e15c9df7383
MJZ1977 <37274752+MJZ1977@users.noreply.github.com>
2019-04-29 14:32:24 +0200

    kingDanger < 560

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -483,7 +483,7 @@ namespace {
     if (kingDanger > 100)
         score -= make_score(kingDanger * kingDanger / 4096, kingDanger / 16);
 
-    inDanger[Us] = kingDanger > 680;
+    inDanger[Us] = kingDanger > 560;
 
     // Penalty when our king is on a pawnless flank
     if (!(pos.pieces(PAWN) & KingFlank[file_of(ksq)]))

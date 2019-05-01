02356ea23b780494e1370cfb1fd655e0de387206
Moez Jellouli <37274752+MJZ1977@users.noreply.github.com>
2019-05-01 15:16:26 +0200

    save

--- src/search.cpp
+++ src/search.cpp
@@ -959,7 +959,8 @@ moves_loop: // When in check, search starts from here
           && bestValue > VALUE_MATED_IN_MAX_PLY)
       {
           // Skip quiet moves if movecount exceeds our FutilityMoveCount threshold
-          moveCountPruning = moveCount >= futility_move_count(improving, depth / ONE_PLY);
+          if (ss->ply > 3)
+			  moveCountPruning = moveCount >= futility_move_count(improving, depth / ONE_PLY);
 
           if (   !captureOrPromotion
               && !givesCheck

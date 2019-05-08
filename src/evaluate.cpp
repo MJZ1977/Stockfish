b90c5114ff9d11c3c91de51658127719e1bb6538
Moez Jellouli <37274752+MJZ1977@users.noreply.github.com>
2019-05-08 18:10:10 +0200

    +2, k < 10

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -669,8 +669,8 @@ namespace {
                 else if (defendedSquares & blockSq)
                     k += 4;
 				
-				else if (!(unsafeSquares & (~defendedSquares | pos.pieces(Them))))
-					k += 3;
+				else if (!(unsafeSquares & (~defendedSquares | pos.pieces(Them))) && k < 10)
+					k += 2;
 
                 bonus += make_score(k * w, k * w);
             }

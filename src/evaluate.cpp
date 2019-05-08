f712dc98500e792f3349bdb712758474159f8a18
Moez Jellouli <37274752+MJZ1977@users.noreply.github.com>
2019-05-08 23:01:04 +0200

    simple increase 20 >> 22

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -659,7 +659,7 @@ namespace {
 
                 // If there aren't any enemy attacks, assign a big bonus. Otherwise
                 // assign a smaller bonus if the block square isn't attacked.
-                int k = !unsafeSquares ? 20 : !(unsafeSquares & blockSq) ? 9 : 0;
+                int k = !unsafeSquares ? 22 : !(unsafeSquares & blockSq) ? 9 : 0;
 
                 // If the path to the queen is fully defended, assign a big bonus.
                 // Otherwise assign a smaller bonus if the block square is defended.
@@ -668,9 +668,6 @@ namespace {
 
                 else if (defendedSquares & blockSq)
                     k += 4;
-				
-				else if (!(unsafeSquares & (~defendedSquares | pos.pieces(Them))) && k < 10)
-					k += 2;
 
                 bonus += make_score(k * w, k * w);
             }

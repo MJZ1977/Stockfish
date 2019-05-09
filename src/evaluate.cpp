89c71a6d08c1cce7f3c87985bb807d23d404d484
MJZ1977 <37274752+MJZ1977@users.noreply.github.com>
2019-05-09 10:34:36 +0200

    (8, 8)

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -287,7 +287,7 @@ namespace {
         if (pos.blockers_for_king(Us) & s)
 		{
             b &= LineBB[pos.square<KING>(Us)][s];
-			score -= make_score(6, 6);
+			score -= make_score(8, 8);
 		}
 
         attackedBy2[Us] |= attackedBy[Us][ALL_PIECES] & b;

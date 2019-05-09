b656756845a611ad8a8eab179eca129d77fc04b4
MJZ1977 <37274752+MJZ1977@users.noreply.github.com>
2019-05-09 16:37:31 +0200

    First try, no MCP for plies 0 and 1

--- src/search.cpp
+++ src/search.cpp
@@ -953,7 +953,8 @@ moves_loop: // When in check, search starts from here
           && bestValue > VALUE_MATED_IN_MAX_PLY)
       {
           // Skip quiet moves if movecount exceeds our FutilityMoveCount threshold
-          moveCountPruning = moveCount >= futility_move_count(improving, depth / ONE_PLY);
+          moveCountPruning = moveCount >= futility_move_count(improving, depth / ONE_PLY)
+                             && ss->ply > 1;
 
           if (   !captureOrPromotion
               && !givesCheck

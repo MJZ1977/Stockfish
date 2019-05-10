9a36ee5d7078fa17bdffefbcb8d48b0f2c7e05f2
MJZ1977 <37274752+MJZ1977@users.noreply.github.com>
2019-05-10 10:00:28 +0200

    Cap to 1

--- src/search.cpp
+++ src/search.cpp
@@ -1025,7 +1025,7 @@ moves_loop: // When in check, search starts from here
 
 		  if (move == ttMove2)
 		  {
-		    r = std::min(r, 2 * ONE_PLY);
+		    r = std::min(r, ONE_PLY);
 			/*pos.undo_move(move);
 			sync_cout << "Position = " << pos.fen()
                         << " TTmove = " << UCI::move(ttMove, pos.is_chess960())

5148d625b866054ba482e11df4cb7a9f3cfe208b
Moez Jellouli <37274752+MJZ1977@users.noreply.github.com>
2019-05-18 11:18:21 +0200

    -107

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -751,7 +751,7 @@ namespace {
                     + 18 * pawnsOnBothFlanks
                     + 49 * !pos.non_pawn_material()
 					+  6 * pe->weak_unopposed(weakSide)
-                    -105 ;
+                    -107 ;
 
     // Now apply the bonus: note that we find the attacking side by extracting
     // the sign of the endgame value, and that we carefully cap the bonus so

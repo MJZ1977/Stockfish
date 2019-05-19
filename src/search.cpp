1d3631293084500d14befed9c7ed484ac646073c
Moez Jellouli <37274752+MJZ1977@users.noreply.github.com>
2019-05-19 23:15:31 +0200

    save

--- src/search.cpp
+++ src/search.cpp
@@ -1017,7 +1017,7 @@ moves_loop: // When in check, search starts from here
 
           // Decrease reduction if position is or has been on the PV
           if (ttPv)
-              r -= 2 * ONE_PLY;
+              r -= ONE_PLY;
 
           // Decrease reduction if opponent's move count is high (~10 Elo)
           if ((ss-1)->moveCount > 15)

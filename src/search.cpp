5b9f6514d9391fea288678648898553ea3bc1142
Moez Jellouli <37274752+MJZ1977@users.noreply.github.com>
2019-05-20 02:13:01 +0200

    Save
    Bench: 3290670

--- src/search.cpp
+++ src/search.cpp
@@ -1008,10 +1008,7 @@ moves_loop: // When in check, search starts from here
       // Step 16. Reduced depth search (LMR). If the move fails high it will be
       // re-searched at full depth.
       if (    depth >= 3 * ONE_PLY
-          &&  moveCount > 1 + 3 * rootNode
-          && (  !captureOrPromotion
-              || moveCountPruning
-              || ss->staticEval + PieceValue[EG][pos.captured_piece()] <= alpha))
+          &&  moveCount > 1 + 3 * rootNode)
       {
           Depth r = reduction(improving, depth, moveCount);
 
@@ -1025,7 +1022,10 @@ moves_loop: // When in check, search starts from here
           // Decrease reduction if move has been singularly extended
           r -= singularExtensionLMRmultiplier * ONE_PLY;
 
-          if (!captureOrPromotion)
+          // Decrease reduction in case of capture or promotions
+          if (captureOrPromotion)
+              r -= ONE_PLY;
+          else
           {
               // Increase reduction if ttMove is a capture (~0 Elo)
               if (ttCapture)

cfd4ab2b2dc78d60a307314376cd95272d925420
Moez Jellouli <37274752+MJZ1977@users.noreply.github.com>
2019-05-22 14:46:39 +0200

    depth < 5

--- src/search.cpp
+++ src/search.cpp
@@ -1080,10 +1080,10 @@ moves_loop: // When in check, search starts from here
           (ss+1)->pv = pv;
           (ss+1)->pv[0] = MOVE_NONE;
 
-          if (alpha > Value(400) 
+          if (alpha > Value(360) 
               && !extension
               && !improving
-              && newDepth < 4 * ONE_PLY
+              && newDepth < 5 * ONE_PLY
               && ss->ply < 3 * thisThread->rootDepth / ONE_PLY)
               newDepth += ONE_PLY;
 

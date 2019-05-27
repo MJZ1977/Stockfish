4921d86127e75e09cdc304831c8e1dfe46764ca7
MJZ1977 <37274752+MJZ1977@users.noreply.github.com>
2019-05-27 13:57:12 +0200

    correction

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -132,10 +132,10 @@ namespace {
             score += make_score(v, v * (r - 2) / 4);
         }
         else if (!neighbours)
-            score -= Isolated, e->weakUnopposed[Us] += !(opposed || r >= RANK_5);
+            score -= Isolated, e->weakUnopposed[Us] += (!opposed || r >= RANK_5);
 
         else if (backward)
-            score -= Backward, e->weakUnopposed[Us] += !(opposed || r >= RANK_5);
+            score -= Backward, e->weakUnopposed[Us] += (!opposed || r >= RANK_5);
 
         if (doubled && !support)
             score -= Doubled;

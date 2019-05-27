0f0ff23beebf9ecd5c3ab0993d8e015fccd47cff
MJZ1977 <37274752+MJZ1977@users.noreply.github.com>
2019-05-27 11:41:34 +0200

    (5, 0)

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -401,9 +401,9 @@ namespace {
 
     // Init the score with king shelter and enemy pawns storm
     Score score = pe->king_safety<Us>(pos);
-	
+
 	// Bonus for blocked storm pawns
-	score += make_score(10, 0) * popcount(KingFlank[file_of(ksq)] & pos.pieces(Them, PAWN) & shift<Up>(pos.pieces(Us)));
+	score += make_score(5, 0) * popcount(KingFlank[file_of(ksq)] & pos.pieces(Them, PAWN) & shift<Up>(pos.pieces(Us)));
 
     // Attacked squares defended at most once by our queen or king
     weak =  attackedBy[Them][ALL_PIECES]

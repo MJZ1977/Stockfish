12cdbb9f1784f895089787b3f6168753125dde62
Moez Jellouli <moez.jellouli@wanadoo.fr>
2019-06-02 12:56:43 +0100

    3 + sq / 2

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -270,6 +270,8 @@ namespace {
     constexpr Direction Down = (Us == WHITE ? SOUTH : NORTH);
     constexpr Bitboard OutpostRanks = (Us == WHITE ? Rank4BB | Rank5BB | Rank6BB
                                                    : Rank5BB | Rank4BB | Rank3BB);
+    constexpr Bitboard TheirCamp = (Us == WHITE ? Rank5BB | Rank6BB | Rank7BB | Rank8BB
+                                                   : Rank1BB | Rank2BB | Rank3BB | Rank4BB);
     const Square* pl = pos.squares<Pt>(Us);
 
     Bitboard b, bb;
@@ -307,7 +309,7 @@ namespace {
             // Bonus if piece is on an outpost square or can reach one
             bb = OutpostRanks & ~pe->pawn_attacks_span(Them);
             if (bb & s)
-                score += Outpost * (Pt == KNIGHT ? 4 : 2)
+                score += Outpost * (Pt == KNIGHT ? (3 + popcount(b & mobilityArea[Us] & TheirCamp) / 2) : 2)
                                  * ((attackedBy[Us][PAWN] & s) ? 2 : 1);
 
             else if (bb &= b & ~pos.pieces(Us))

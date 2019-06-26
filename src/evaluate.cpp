15f099ebe1d74b8fd1af2735cfb0b696d7cad7df
Moez Jellouli <37274752+MJZ1977@users.noreply.github.com>
2019-06-26 20:40:36 +0200

    -6 / 48 / -4 / 2

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -314,7 +314,7 @@ namespace {
         mobility[Us] += MobilityBonus[Pt - 2][mob];
 		
 		int dynPSQT = int(pos.this_thread()->dynPSQT[pos.piece_on(s)][s]); 
-	    score += make_score(1,1) * clamp(dynPSQT / 48, -4 , 4);
+	    score += make_score(1,1) * clamp((dynPSQT - 6) / 48, -4 , 2);
 
         if (Pt == BISHOP || Pt == KNIGHT)
         {

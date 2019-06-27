d44ec7b9ef66d8452d803b151b18c3cc71bb76e8
Moez Jellouli <37274752+MJZ1977@users.noreply.github.com>
2019-06-27 22:58:31 +0200

    MG+16, EG-5

--- src/types.h
+++ src/types.h
@@ -186,7 +186,7 @@ enum Value : int {
   RookValueMg   = 1289,  RookValueEg   = 1378,
   QueenValueMg  = 2529,  QueenValueEg  = 2687,
 
-  MidgameLimit  = 15275, EndgameLimit  = 3900
+  MidgameLimit  = 15275, EndgameLimit  = 3910
 };
 
 enum PieceType {

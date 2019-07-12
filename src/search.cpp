ba52c5de21250b4877553cc8d280fe6f666c6562
MJZ1977 <37274752+MJZ1977@users.noreply.github.com>
2019-07-12 10:50:26 +0200

    Threshold 12

--- src/search.cpp
+++ src/search.cpp
@@ -951,7 +951,7 @@ moves_loop: // When in check, search starts from here
           &&  tte->depth() >= depth - 3 * ONE_PLY
           &&  pos.legal(move))
       {
-          Value singularBeta = ttValue - Value(20);
+          Value singularBeta = ttValue - Value(12);
           Depth halfDepth = depth / (2 * ONE_PLY) * ONE_PLY; // ONE_PLY invariant
           ss->excludedMove = move;
           value = search<NonPV>(pos, ss, singularBeta - 1, singularBeta, halfDepth, cutNode);

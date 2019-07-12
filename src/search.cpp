8ad758860c07cf2f7ad8487b3ceab6ed801cb14a
Moez Jellouli <37274752+MJZ1977@users.noreply.github.com>
2019-07-13 00:43:32 +0200

    upper and lower bounds +/- 3 delta
    Bench 3231480

--- src/search.cpp
+++ src/search.cpp
@@ -444,7 +444,7 @@ void Thread::search() {
               // re-search, otherwise exit the loop.
               if (bestValue <= alpha)
               {
-                  beta = (alpha + beta) / 2;
+                  beta = std::min((alpha + beta) / 2, bestValue + 3 * delta);
                   alpha = std::max(bestValue - delta, -VALUE_INFINITE);
 
                   failedHighCnt = 0;
@@ -454,7 +454,7 @@ void Thread::search() {
               else if (bestValue >= beta)
               {
                   beta = std::min(bestValue + delta, VALUE_INFINITE);
-                  alpha = std::max(alpha, bestValue - 4 * delta);
+                  alpha = std::max(alpha, bestValue - 3 * delta);
                   ++failedHighCnt;
               }
               else

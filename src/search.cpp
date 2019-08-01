18fed94fb4a7fa4223bbaf219bd59814d79f517b
MJZ1977 <37274752+MJZ1977@users.noreply.github.com>
2019-08-01 17:36:54 +0200

    cap to -80

--- src/search.cpp
+++ src/search.cpp
@@ -798,7 +798,7 @@ namespace {
         && (ss-1)->currentMove != MOVE_NULL
         && (ss-1)->statScore < 22661
         &&  eval >= beta
-        &&  ss->staticEval >= beta + std::max(225 - 36 * depth / ONE_PLY, 0)
+        &&  ss->staticEval >= beta + std::max(225 - 36 * depth / ONE_PLY, -80)
         && !excludedMove
         &&  pos.non_pawn_material(us)
         && (ss->ply >= thisThread->nmpMinPly || us != thisThread->nmpColor))

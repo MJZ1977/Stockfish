ed40380a6eb0b0bf43e122e10b078ca4c9f84e4f
Moez Jellouli <37274752+MJZ1977@users.noreply.github.com>
2019-08-01 22:48:46 +0200

    Revert "remove condition"
    This reverts commit 652c24f300117632117248b17dca322f22b06a30.

--- src/search.cpp
+++ src/search.cpp
@@ -798,7 +798,7 @@ namespace {
         && (ss-1)->currentMove != MOVE_NULL
         && (ss-1)->statScore < 22661
         &&  eval >= beta
-        &&  ss->staticEval >= beta
+        &&  ss->staticEval >= beta + std::max(225 - 36 * depth / ONE_PLY, 0)
         && !excludedMove
         &&  pos.non_pawn_material(us)
         && (ss->ply >= thisThread->nmpMinPly || us != thisThread->nmpColor))

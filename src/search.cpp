adf1bbc943fba90cee4ed2c6873dbbb661b52d65
Moez Jellouli <37274752+MJZ1977@users.noreply.github.com>
2019-08-04 20:54:31 +0200

    save tune

--- src/search.cpp
+++ src/search.cpp
@@ -798,7 +798,7 @@ namespace {
         && (ss-1)->currentMove != MOVE_NULL
         && (ss-1)->statScore < 22661
         &&  eval >= beta
-        &&  ss->staticEval >= std::min(beta - 33 * depth / ONE_PLY + 180 + ss->ply * 4, -(ss-1)->staticEval + 160)
+        &&  ss->staticEval >= std::min(beta - 32 * depth / ONE_PLY + 184 + ss->ply * 5, -(ss-1)->staticEval + 154)
         && !excludedMove
         &&  pos.non_pawn_material(us)
         && (ss->ply >= thisThread->nmpMinPly || us != thisThread->nmpColor))

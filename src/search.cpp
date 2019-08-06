45bfa1a4ea43573ca6bb153e5f8dd4741f896431
MJZ1977 <37274752+MJZ1977@users.noreply.github.com>
2019-08-06 17:22:01 +0200

    thres 140

--- src/search.cpp
+++ src/search.cpp
@@ -798,7 +798,7 @@ namespace {
         && (ss-1)->currentMove != MOVE_NULL
         && (ss-1)->statScore < 22661
         &&  eval >= beta
-        &&  ss->staticEval >= std::min(beta - 33 * depth / ONE_PLY + 299, -(ss-1)->staticEval + 160)
+        &&  ss->staticEval >= std::min(beta - 33 * depth / ONE_PLY + 299, -(ss-1)->staticEval + 140)
         && !excludedMove
         &&  pos.non_pawn_material(us)
         && (ss->ply >= thisThread->nmpMinPly || us != thisThread->nmpColor))

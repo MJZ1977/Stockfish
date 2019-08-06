c53605f76833f0693e1fd80a792368ae903b0215
MJZ1977 <37274752+MJZ1977@users.noreply.github.com>
2019-08-06 09:50:06 +0200

    400 / 290

--- src/search.cpp
+++ src/search.cpp
@@ -798,7 +798,7 @@ namespace {
         && (ss-1)->currentMove != MOVE_NULL
         && (ss-1)->statScore < 22661
         &&  eval >= beta
-        &&  ss->staticEval >= beta - (330 * depth) / thisThread->rootDepth + 240
+        &&  ss->staticEval >= beta - (400 * depth) / thisThread->rootDepth + 290
         && !excludedMove
         &&  pos.non_pawn_material(us)
         && (ss->ply >= thisThread->nmpMinPly || us != thisThread->nmpColor))

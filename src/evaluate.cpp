c6ab3d11fd10f827f81aecc338f1d910692e6284
MJZ1977 <37274752+MJZ1977@users.noreply.github.com>
2019-08-09 17:55:01 +0200

    save tune

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -40,6 +40,12 @@ namespace Trace {
 
   Score scores[TERM_NB][COLOR_NB];
 
+  int AA = 35;
+  int BB = 20;
+  int CC = 9;
+  int DD = 5;
+  TUNE(AA, BB, CC, DD);
+
   double to_cp(Value v) { return double(v) / PawnValueEg; }
 
   void add(int idx, Color c, Score s) {
@@ -644,14 +650,14 @@ namespace {
                 // If there are no enemy attacks on passed pawn span, assign a big bonus.
                 // Otherwise assign a smaller bonus if the path to queen is not attacked
                 // and even smaller bonus if it is attacked but block square is not.
-                int k = !unsafeSquares                    ? 35 :
-                        !(unsafeSquares & squaresToQueen) ? 20 :
-                        !(unsafeSquares & blockSq)        ?  9 :
+                int k = !unsafeSquares                    ? AA :
+                        !(unsafeSquares & squaresToQueen) ? BB :
+                        !(unsafeSquares & blockSq)        ? CC :
                                                              0 ;
 
                 // Assign a larger bonus if the block square is defended
                 if ((pos.pieces(Us) & bb) || (attackedBy[Us][ALL_PIECES] & blockSq))
-                    k += 5;
+                    k += DD;
 
                 bonus += make_score(k * w, k * w);
             }

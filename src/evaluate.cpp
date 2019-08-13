a39f4a216ce5ed06dfe974d60336b1ce48a5b202
MJZ1977 <37274752+MJZ1977@users.noreply.github.com>
2019-08-13 15:23:03 +0200

    save

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -130,7 +130,7 @@ namespace {
   constexpr Score BishopPawns        = S(  3,  7);
   constexpr Score CorneredBishop     = S( 50, 50);
   constexpr Score FlankAttacks       = S(  8,  0);
-  constexpr Score Hanging            = S( 69, 36);
+  Score Hanging            = S( 69, 36);
   constexpr Score KingProtector      = S(  7,  8);
   constexpr Score KnightOnQueen      = S( 16, 12);
   constexpr Score LongDiagonalBishop = S( 45,  0);
@@ -147,7 +147,8 @@ namespace {
   constexpr Score ThreatBySafePawn   = S(173, 94);
   constexpr Score TrappedRook        = S( 47,  4);
   constexpr Score WeakQueen          = S( 49, 15);
-  constexpr Score WeakPiece          = S(  5,  5);
+  Score WeakPiece          = S(  5,  5);
+  TUNE(Hanging, WeakPiece);
 
 #undef S
 

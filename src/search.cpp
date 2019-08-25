ef4c8384ccbb208d020f92d15d0b21dcd5bfb495
Moez Jellouli <37274752+MJZ1977@users.noreply.github.com>
2019-08-25 22:33:24 +0200

    save ttMove if no bestMove

--- src/search.cpp
+++ src/search.cpp
@@ -1292,7 +1292,7 @@ moves_loop: // When in check, search starts from here
         tte->save(posKey, value_to_tt(bestValue, ss->ply), ttPv,
                   bestValue >= beta ? BOUND_LOWER :
                   PvNode && bestMove ? BOUND_EXACT : BOUND_UPPER,
-                  depth, bestMove, ss->staticEval);
+                  depth, bestMove? bestMove : ttMove, ss->staticEval);
 
     assert(bestValue > -VALUE_INFINITE && bestValue < VALUE_INFINITE);
 

9a44eebefa9db6aebc23b880330e59d8a4710b3b
MJZ1977 <37274752+MJZ1977@users.noreply.github.com>
2019-08-30 12:05:51 +0200

    no ttMove penalty

--- src/search.cpp
+++ src/search.cpp
@@ -687,12 +687,12 @@ namespace {
                     update_continuation_histories(ss-1, pos.piece_on(prevSq), prevSq, -stat_bonus(depth + ONE_PLY));
             }
             // Penalty for a quiet ttMove that fails low
-            else if (!pos.capture_or_promotion(ttMove))
+          /*  else if (!pos.capture_or_promotion(ttMove))
             {
                 int penalty = -stat_bonus(depth);
                 thisThread->mainHistory[us][from_to(ttMove)] << penalty;
                 update_continuation_histories(ss, pos.moved_piece(ttMove), to_sq(ttMove), penalty);
-            }
+            }*/
         }
         return ttValue;
     }
@@ -1298,6 +1298,11 @@ moves_loop: // When in check, search starts from here
                   bestValue >= beta ? BOUND_LOWER :
                   PvNode && bestMove ? BOUND_EXACT : BOUND_UPPER,
                   depth, bestMove2, ss->staticEval);
+    /*if (bestMove2 && !(bestMove))
+      sync_cout << "Position = " << pos.fen()
+                << " - depth = " << depth / ONE_PLY
+                << " - Move2 = " << UCI::move(bestMove2, pos.is_chess960())
+                << " - ttMove = " << UCI::move(ttMove, pos.is_chess960()) << sync_endl;*/
 
     assert(bestValue > -VALUE_INFINITE && bestValue < VALUE_INFINITE);
 

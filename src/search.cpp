23a699763aa7eb02d8cd6e8b253729d63b180fde
MJZ1977 <37274752+MJZ1977@users.noreply.github.com>
2019-08-30 17:23:19 +0200

    -200 + 33x ply

--- src/search.cpp
+++ src/search.cpp
@@ -803,7 +803,7 @@ namespace {
         && (ss-1)->statScore < 22661
         &&  eval >= beta
         &&  eval >= ss->staticEval
-        &&  ss->staticEval >= beta - 33 * depth / ONE_PLY + 299 - improving * 30
+        &&  ss->staticEval >= beta - 200 + 33 * ss->ply - improving * 30
         && !excludedMove
         &&  pos.non_pawn_material(us)
         && (ss->ply >= thisThread->nmpMinPly || us != thisThread->nmpColor))

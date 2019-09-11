148b1cdbab944696a9772e2c126cddf965c5e8da
Moez Jellouli <37274752+MJZ1977@users.noreply.github.com>
2019-09-12 00:44:53 +0200

    (22,0)

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -580,7 +580,7 @@ namespace {
     {
         b = pos.pieces(Them,BISHOP) & (Them == WHITE ? Rank1BB : Rank8BB);
         if (b)
-           score += make_score(16, 0) * popcount(pos.pieces(Them,PAWN) & weak & pawn_attacks_bb<Them>(b));
+           score += make_score(22, 0) * popcount(pos.pieces(Them,PAWN) & weak & pawn_attacks_bb<Them>(b));
     }
 
     if (T)

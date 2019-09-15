619473361181f7aa368b7c55e166ec0ca9db43e2
Moez Jellouli <37274752+MJZ1977@users.noreply.github.com>
2019-09-15 17:03:47 +0200

    save 28-12-40

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -764,8 +764,11 @@ namespace {
         if (   pos.opposite_bishops()
             && pos.non_pawn_material() == 2 * BishopValueMg)
             sf = 16 + 4 * pe->passed_count();
+        else if (pos.count<PAWN>(strongSide) == 0 && pos.count<ROOK>(strongSide) == 0 && pos.count<QUEEN>(strongSide) == 0)
+            sf = 8;
         else
-            sf = std::min(40 + (pos.opposite_bishops() ? 2 : 7) * pos.count<PAWN>(strongSide), sf);
+            sf = std::min(28 + ((pos.opposite_bishops() ? 12 : 40) * pos.count<PAWN>(strongSide)) / (1+pos.count<PAWN>(strongSide))
+                             + 2 * pos.count<QUEEN>(strongSide) + 2 * pos.count<ROOK>(strongSide), sf);
 
     }
 

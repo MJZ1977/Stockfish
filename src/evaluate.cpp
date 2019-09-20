d432a9d66c8486a93743f3f366dcd2046fe1f6d0
Moez Jellouli <37274752+MJZ1977@users.noreply.github.com>
2019-09-20 18:21:21 +0200

    4 / 1

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -650,9 +650,9 @@ namespace {
 
                 // Assign a larger bonus if the block square is defended
                 if ((pos.pieces(Us) & bb) || (attackedBy[Us][ALL_PIECES] & blockSq))
-                    k += 5;
+                    k += 4;
                 if (attackedBy[Us][PAWN] & blockSq)
-                    k += 2;
+                    k += 1;
 
                 bonus += make_score(k * w, k * w);
             }

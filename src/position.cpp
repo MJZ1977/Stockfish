05d61e530e45d4887463cd9d06f966385896a711
MJZ1977 <37274752+MJZ1977@users.noreply.github.com>
2019-09-23 13:10:19 +0200

    39 to 49

--- src/position.cpp
+++ src/position.cpp
@@ -1127,9 +1127,9 @@ bool Position::see_ge(Move m, Value threshold) const {
 /// Position::is_draw() tests whether the position is drawn by 50-move rule
 /// or by repetition. It does not detect stalemates.
 
-bool Position::is_draw(int ply) const {
+bool Position::is_draw(int ply, int suffle_limit) const {
 
-  if (st->rule50 > 99 && (!checkers() || MoveList<LEGAL>(*this).size()))
+  if (std::min(st->rule50, ply) > std::min(suffle_limit, 99) && (!checkers() || MoveList<LEGAL>(*this).size()))
       return true;
 
   // Return a draw score if a position repeats once earlier but strictly
--- src/position.h
+++ src/position.h
@@ -154,7 +154,7 @@ public:
   int game_ply() const;
   bool is_chess960() const;
   Thread* this_thread() const;
-  bool is_draw(int ply) const;
+  bool is_draw(int ply, int suffle_limit) const;
   bool has_game_cycle(int ply) const;
   bool has_repeated() const;
   int rule50_count() const;

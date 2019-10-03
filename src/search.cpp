f895007dbb54b30b752584f8a4e68eb9aa555c05
MJZ1977 <37274752+MJZ1977@users.noreply.github.com>
2019-10-03 11:22:26 +0200

    save

--- src/search.cpp
+++ src/search.cpp
@@ -328,7 +328,7 @@ void Thread::search() {
   Move  pv[MAX_PLY+1];
   Value bestValue, alpha, beta, delta;
   Move  lastBestMove = MOVE_NONE;
-  Move  lastBestMove2 = MOVE_NONE;
+  Move  lastBestCMove = MOVE_NONE;
   Depth lastBestMoveDepth = DEPTH_ZERO;
   MainThread* mainThread = (this == Threads.main() ? Threads.main() : nullptr);
   double timeReduction = 1, totBestMoveChanges = 0;
@@ -495,7 +495,7 @@ void Thread::search() {
 
       if (rootMoves[0].pv[0] != lastBestMove) {
          lastBestMove = rootMoves[0].pv[0];
-         lastBestMove2 = rootMoves[0].pv[1];
+         lastBestCMove = rootMoves[0].pv[1];
          lastBestMoveDepth = rootDepth;
       }
 
@@ -522,8 +522,8 @@ void Thread::search() {
 
           // If the bestMove is stable over several iterations, reduce time accordingly
           timeReduction = lastBestMoveDepth + 9 * ONE_PLY < completedDepth ? 1.97 : 0.98;
-          if (rootMoves[0].pv[1] != lastBestMove2 && timeReduction > 1.5)
-             timeReduction = 1.5;
+          if (rootMoves[0].pv[1] != lastBestCMove && timeReduction > 1.7)
+             timeReduction = 1.7;
           double reduction = (1.36 + mainThread->previousTimeReduction) / (2.29 * timeReduction);
 
           // Use part of the gained time from a previous stable move for the current move

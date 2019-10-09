c2bebb84ea727ebe3802e5dd37d3e80d34c70ea9
MJZ1977 <37274752+MJZ1977@users.noreply.github.com>
2019-10-09 15:00:08 +0200

    complexity -10

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -723,7 +723,7 @@ namespace {
                     + 18 * pawnsOnBothFlanks
                     + 49 * !pos.non_pawn_material()
                     - 36 * almostUnwinnable
-                    - 20 * pe->almostBlocked
+                    - 10 * pe->almostBlocked
                     -103 ;
 
     // Now apply the bonus: note that we find the attacking side by extracting the

9325ea5c059112cfd8a4ddf22d73c92d8bf27a76
Moez Jellouli <37274752+MJZ1977@users.noreply.github.com>
2019-10-09 21:38:48 +0200

    kingdanger threshold +40

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -461,7 +461,7 @@ namespace {
                  -   7;
 
     // Transform the kingDanger units into a Score, and subtract it from the evaluation
-    if (kingDanger > 100)
+    if (kingDanger > 100 + 40 * pe->almostBlocked)
         score -= make_score(kingDanger * kingDanger / 4096, kingDanger / 16);
 
     // Penalty when our king is on a pawnless flank

c45d668dd58485c22dec34d116e8c04d24b18da1
Moez Jellouli <37274752+MJZ1977@users.noreply.github.com>
2019-10-18 22:28:42 +0200

    simple checks, 200

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -109,7 +109,7 @@ void MovePicker::score() {
       if (Type == CAPTURES)
           m.value =  int(PieceValue[MG][pos.piece_on(to_sq(m))]) * 6
                    + (*captureHistory)[pos.moved_piece(m)][to_sq(m)][type_of(pos.piece_on(to_sq(m)))]
-                   + 40 * pos.gives_check(m);
+                   + 200 * bool(pos.check_squares(type_of(pos.moved_piece(m))) & to_sq(m));
 
       else if (Type == QUIETS)
           m.value =      (*mainHistory)[pos.side_to_move()][from_to(m)]
@@ -117,7 +117,7 @@ void MovePicker::score() {
                    + 2 * (*continuationHistory[1])[pos.moved_piece(m)][to_sq(m)]
                    + 2 * (*continuationHistory[3])[pos.moved_piece(m)][to_sq(m)]
                    +     (*continuationHistory[5])[pos.moved_piece(m)][to_sq(m)]
-                   + 40 * pos.gives_check(m);
+                   + 200 * bool(pos.check_squares(type_of(pos.moved_piece(m))) & to_sq(m));
 
       else // Type == EVASIONS
       {

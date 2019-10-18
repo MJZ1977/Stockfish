5f38c241cf943b49ab4bdd21966c5d75a6b4c0b6
Moez Jellouli <37274752+MJZ1977@users.noreply.github.com>
2019-10-18 19:27:32 +0200

    without depth limit

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -109,7 +109,7 @@ void MovePicker::score() {
       if (Type == CAPTURES)
           m.value =  int(PieceValue[MG][pos.piece_on(to_sq(m))]) * 6
                    + (*captureHistory)[pos.moved_piece(m)][to_sq(m)][type_of(pos.piece_on(to_sq(m)))]
-                   + 200 * (stage != PROBCUT_INIT && depth < 6 && pos.gives_check(m));
+                   + 200 * pos.gives_check(m);
 
       else if (Type == QUIETS)
           m.value =      (*mainHistory)[pos.side_to_move()][from_to(m)]
@@ -117,7 +117,7 @@ void MovePicker::score() {
                    + 2 * (*continuationHistory[1])[pos.moved_piece(m)][to_sq(m)]
                    + 2 * (*continuationHistory[3])[pos.moved_piece(m)][to_sq(m)]
                    +     (*continuationHistory[5])[pos.moved_piece(m)][to_sq(m)]
-                   + 200 * (stage != PROBCUT_INIT && depth < 6 && pos.gives_check(m));
+                   + 200 * pos.gives_check(m);
 
       else // Type == EVASIONS
       {

3c3dde24a78b098cb40860204145910a4c108fb3
MJZ1977 <37274752+MJZ1977@users.noreply.github.com>
2019-10-24 10:35:22 +0200

    rank(to-sq) x 100

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -21,6 +21,7 @@
 #include <cassert>
 
 #include "movepick.h"
+#include "pawns.h"
 
 namespace {
 
@@ -105,7 +106,10 @@ void MovePicker::score() {
 
   static_assert(Type == CAPTURES || Type == QUIETS || Type == EVASIONS, "Wrong type");
 
+  Bitboard passedPawns = (Pawns::probe(pos))->passed_pawns(pos.side_to_move());
+
   for (auto& m : *this)
+  {
       if (Type == CAPTURES)
           m.value =  int(PieceValue[MG][pos.piece_on(to_sq(m))]) * 6
                    + (*captureHistory)[pos.moved_piece(m)][to_sq(m)][type_of(pos.piece_on(to_sq(m)))];
@@ -127,6 +131,9 @@ void MovePicker::score() {
                        + (*continuationHistory[0])[pos.moved_piece(m)][to_sq(m)]
                        - (1 << 28);
       }
+      if (passedPawns & from_sq(m))
+         m.value += int(relative_rank(pos.side_to_move(), to_sq(m))) * 100;
+  }
 }
 
 /// MovePicker::select() returns the next move satisfying a predicate function.

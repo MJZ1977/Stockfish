3f813ad8907f472f9556fe052a15a3653c307265
MJZ1977 <37274752+MJZ1977@users.noreply.github.com>
2019-11-25 10:03:19 +0100

    1000 + lastcapture MG
    bench 5603943

--- src/search.cpp
+++ src/search.cpp
@@ -1052,7 +1052,7 @@ moves_loop: // When in check, search starts from here
       // Last captures extension
       else if (   PvNode
                && PieceValue[EG][pos.captured_piece()] > PawnValueEg
-               && pos.non_pawn_material() < 2600)
+               && pos.non_pawn_material() < 1000 + PieceValue[MG][pos.captured_piece()])
           extension = 1;
 
       // Castling extension

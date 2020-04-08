5ae42c5706da1d98a04e2a4778aafac121a5f660
Moez Jellouli <37274752+MJZ1977@users.noreply.github.com>
2020-04-08 10:53:01 +0200

    d > 13

--- src/search.cpp
+++ src/search.cpp
@@ -1166,7 +1166,7 @@ moves_loop: // When in check, search starts from here
           if (singularLMR)
               r -= 1 + formerPv;
 
-          if (pos.non_pawn_material() <= 2 * BishopValueMg && PvNode && depth > 12)
+          if (pos.non_pawn_material() <= 2 * BishopValueMg && PvNode && depth > 13)
               r++; 
 
           if (!captureOrPromotion)

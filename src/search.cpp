bcff8539a1c373c738376402588577d8c156d010
Moez Jellouli <37274752+MJZ1977@users.noreply.github.com>
2020-04-08 10:50:08 +0200

    NPM < 2B, d > 12

--- src/search.cpp
+++ src/search.cpp
@@ -1166,6 +1166,9 @@ moves_loop: // When in check, search starts from here
           if (singularLMR)
               r -= 1 + formerPv;
 
+          if (pos.non_pawn_material() <= 2 * BishopValueMg && PvNode && depth > 12)
+              r++; 
+
           if (!captureOrPromotion)
           {
               // Increase reduction if ttMove is a capture (~5 Elo)

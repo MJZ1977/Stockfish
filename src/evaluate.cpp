48f95116ec16868eeb0fcb230d5572ce9d4af8be
Moez Jellouli <37274752+MJZ1977@users.noreply.github.com>
2020-04-09 22:24:15 +0200

    -22

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -717,7 +717,7 @@ namespace {
                     + 24 * infiltration
                     + 51 * !pos.non_pawn_material()
                     - 43 * almostUnwinnable
-                    - 16 * !rookOrQueen
+                    - 22 * !rookOrQueen
                     -108 ;
 
     Value mg = mg_value(score);

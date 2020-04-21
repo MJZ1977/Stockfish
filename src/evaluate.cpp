a75183a52a8ed0725182c2c5088df3a527840ba3
Moez Jellouli <37274752+MJZ1977@users.noreply.github.com>
2020-04-21 12:26:51 +0200

    blok sq attacked by 2

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -649,7 +649,7 @@ namespace {
 
                 // Assign a larger bonus if the block square is defended
                 if ((pos.pieces(Us) & bb) || (attackedBy[Us][ALL_PIECES] & blockSq))
-                    k += 5;
+                    k += 6 - 2 * bool(attackedBy2[Them] & blockSq);
 
                 bonus += make_score(k * w, k * w);
             }

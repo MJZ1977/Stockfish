4337f06be0e40778487ef1a59ac7f4129b082a00
Moez Jellouli <37274752+MJZ1977@users.noreply.github.com>
2020-04-23 16:55:03 +0200

    1800-30 x RD

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -74,7 +74,7 @@ using namespace Trace;
 namespace {
 
   // Threshold for lazy and space evaluation
-  constexpr Value LazyThreshold  = Value(1600);
+  constexpr Value LazyThreshold  = Value(1800);
   constexpr Value SpaceThreshold = Value(12222);
 
   // KingAttackWeights[PieceType] contains king attack weights by piece type
@@ -807,7 +807,7 @@ namespace {
 
     // Early exit if score is high
     Value v = (mg_value(score) + eg_value(score)) / 2;
-    if (abs(v) > LazyThreshold + pos.non_pawn_material() / 64 - 10 * int(pos.this_thread()->rootDepth))
+    if (abs(v) > LazyThreshold + pos.non_pawn_material() / 64 - 30 * int(pos.this_thread()->rootDepth))
        return pos.side_to_move() == WHITE ? v : -v;
 
     // Main evaluation begins here

ec46b8bdb69b121f68a55f580ae8a5d342b886a4
Moez Jellouli <37274752+MJZ1977@users.noreply.github.com>
2020-04-25 22:51:54 +0200

    -1/8

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -821,7 +821,7 @@ namespace {
             + pieces<WHITE, ROOK  >() - pieces<BLACK, ROOK  >()
             + pieces<WHITE, QUEEN >() - pieces<BLACK, QUEEN >();
 
-    score += mobility[WHITE] - mobility[BLACK];
+    score += (mobility[WHITE] - mobility[BLACK]) * 7 / 8;
 
     score +=  king<   WHITE>() - king<   BLACK>()
             + threats<WHITE>() - threats<BLACK>()

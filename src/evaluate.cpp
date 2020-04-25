0694ee735658d7460c3e89f5b3d84a2f166c6554
Moez Jellouli <37274752+MJZ1977@users.noreply.github.com>
2020-04-25 23:39:31 +0200

    (12+max(blocked,4)) / 16

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -821,7 +821,7 @@ namespace {
             + pieces<WHITE, ROOK  >() - pieces<BLACK, ROOK  >()
             + pieces<WHITE, QUEEN >() - pieces<BLACK, QUEEN >();
 
-    score += (mobility[WHITE] - mobility[BLACK]) * 7 / 8;
+    score += (mobility[WHITE] - mobility[BLACK]) * (12 + std::max(pe->blocked_count(), 4)) / 16;
 
     score +=  king<   WHITE>() - king<   BLACK>()
             + threats<WHITE>() - threats<BLACK>()

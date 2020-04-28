3347eed74a5e24cccc484c43ea20039defae4776
Moez Jellouli <37274752+MJZ1977@users.noreply.github.com>
2020-04-28 23:46:59 +0200

    save

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -81,10 +81,11 @@ namespace {
   constexpr int KingAttackWeights[PIECE_TYPE_NB] = { 0, 0, 81, 52, 44, 10 };
 
   // Penalties for enemy's safe checks
-  constexpr int QueenSafeCheck  = 780;
-  constexpr int RookSafeCheck   = 1078;
-  constexpr int BishopSafeCheck = 635;
-  constexpr int KnightSafeCheck = 790;
+  int QueenSafeCheck  = 680;
+  int RookSafeCheck   = 978;
+  int BishopSafeCheck = 735;
+  int KnightSafeCheck = 690;
+  TUNE(QueenSafeCheck, RookSafeCheck, BishopSafeCheck, KnightSafeCheck);
 
 #define S(mg, eg) make_score(mg, eg)
 

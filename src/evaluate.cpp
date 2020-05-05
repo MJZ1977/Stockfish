cec855aa5c4c94406fd5aa9a37f9591f9d42677c
Moez Jellouli <37274752+MJZ1977@users.noreply.github.com>
2020-05-06 00:15:23 +0200

    attackersweight +6

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -324,9 +324,8 @@ namespace {
                     score += LongDiagonalBishop;
 
                 // Bonus for bishop controlling several squares with no opposed bishop
-                if (  !bool(SameColorSq & pos.pieces(Them,BISHOP))
-                   && !bool(SameColorSq & pos.pieces(Them,PAWN) & attackedBy[Them][KING]))
-                    score += make_score(18,0) * popcount(b & attackedBy[Them][KING]);
+                if (  !bool(SameColorSq & pos.pieces(Them,BISHOP)))
+                    kingAttackersWeight[Us] += 6 * popcount(b & attackedBy[Them][KING]);
 
                 // An important Chess960 pattern: a cornered bishop blocked by a friendly
                 // pawn diagonally in front of it is a very serious problem, especially

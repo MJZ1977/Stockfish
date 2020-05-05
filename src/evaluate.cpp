1e4b4a2f9cf340770437d4a4140f3a668e2afdb7
Moez Jellouli <37274752+MJZ1977@users.noreply.github.com>
2020-05-05 23:26:26 +0200

    (12, 0)

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -326,7 +326,7 @@ namespace {
                 // Bonus for bishop controlling several squares with no opposed bishop
                 if (  !bool(SameColorSq & pos.pieces(Them,BISHOP))
                    && !bool(SameColorSq & pos.pieces(Them,PAWN) & attackedBy[Them][KING]))
-                    score += make_score(18,0) * popcount(b & attackedBy[Them][KING]);
+                    score += make_score(12,0) * popcount(b & attackedBy[Them][KING]);
 
                 // An important Chess960 pattern: a cornered bishop blocked by a friendly
                 // pawn diagonally in front of it is a very serious problem, especially

bf7783eda737c5ef4fffe481f75726bef24eb16d
Moez Jellouli <37274752+MJZ1977@users.noreply.github.com>
2020-05-15 17:45:21 +0200

    (4, 8)

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -309,7 +309,7 @@ namespace {
             score -= (Pt == KNIGHT ? KnightKingProtector
                                    : BishopKingProtector) * distance(pos.square<KING>(Us), s);
             if (pe->blocked_count() > 6 && Pt == KNIGHT)
-                score += make_score(6, 6);
+                score += make_score(4, 8);
             
             if (Pt == BISHOP)
             {

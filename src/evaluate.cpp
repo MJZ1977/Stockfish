f53fdf62372bd985de582c6f2ce5963ba6e02c8c
Moez Jellouli <37274752+MJZ1977@users.noreply.github.com>
2020-05-16 22:51:03 +0200

    save

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -349,7 +349,7 @@ namespace {
 
             // Bonus for rook on an open or semi-open file
             if (pos.is_on_semiopen_file(Us, s))
-                score += RookOnFile[pos.is_on_semiopen_file(Them, s)];
+                score += RookOnFile[pos.is_on_semiopen_file(Them, s) && !bool(pos.pieces(Them) & attackedBy[Them][PAWN])];
 
             // Penalty when trapped by the king, even more if the king cannot castle
             else if (mob <= 3)

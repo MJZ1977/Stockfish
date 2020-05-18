6fe2901f814b3e68e397df3d641172f5a50293ee
Moez Jellouli <37274752+MJZ1977@users.noreply.github.com>
2020-05-18 15:51:15 +0200

    rule50 > 8

--- src/search.cpp
+++ src/search.cpp
@@ -1176,7 +1176,7 @@ moves_loop: // When in check, search starts from here
 
           // Decrease reduction if the ttHit running average is large
           if (thisThread->ttHitAverage > 500 * TtHitAverageResolution * TtHitAverageWindow / 1024)
-              r -= 1 + (ss->ply < 2 && pos.rule50_count() > 16);
+              r -= 1 + (ss->ply < 2 && pos.rule50_count() > 8);
 
           // Reduction if other threads are searching this position.
           if (th.marked())

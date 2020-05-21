9a1472acee5b81937c4cfd03b6de8393af350e4e
Moez Jellouli <37274752+MJZ1977@users.noreply.github.com>
2020-05-21 11:25:32 +0200

    tuned values

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -38,8 +38,7 @@ namespace {
   constexpr Score Isolated        = S( 5, 15);
   constexpr Score WeakLever       = S( 0, 56);
   constexpr Score WeakUnopposed   = S(13, 27);
-  Score BlockedStorm[RANK_NB]  = {S( 0,  0), S( 0,  0), S( 82,  82), S( 0,  0), S( 0,  0), S( 0,  0), S( 0,  0)};
-  TUNE(SetRange(-100,100), BlockedStorm);
+  Score BlockedStorm[RANK_NB]  = {S( 0,  0), S( 0,  0), S( 87,  81), S( 4,  3), S( -4,  2), S( 11, -4), S( 0,  0)};
 
   // Connected pawn bonus
   constexpr int Connected[RANK_NB] = { 0, 7, 8, 12, 29, 48, 86 };

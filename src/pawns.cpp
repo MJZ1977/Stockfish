7c8436de042536e233353875d7cdd6ad6f996a1e
Moez Jellouli <37274752+MJZ1977@users.noreply.github.com>
2020-05-23 23:03:33 +0200

    rebase correction

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -35,7 +35,6 @@ namespace {
   constexpr Score Backward        = S( 9, 24);
   constexpr Score BlockedStorm    = S(74, 82);
   constexpr Score Doubled         = S(11, 56);
-  constexpr Score DoubledIsolated = S(15, 57);
   constexpr Score Isolated        = S( 5, 15);
   constexpr Score WeakLever       = S( 0, 56);
   constexpr Score WeakUnopposed   = S(13, 27);

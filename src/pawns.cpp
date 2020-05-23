22a0bba364210df2195a7e184f7be58c8f6e5c2b
Moez Jellouli <37274752+MJZ1977@users.noreply.github.com>
2020-05-23 16:23:46 +0200

    +mg

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -38,7 +38,7 @@ namespace {
   constexpr Score Isolated        = S( 5, 15);
   constexpr Score WeakLever       = S( 0, 56);
   constexpr Score WeakUnopposed   = S(13, 27);
-  constexpr Score BlockedStorm[RANK_NB]  = {S( 0,  0), S( -6,  3), S( 75,  79), S(5,  -12), S( 9,  6), S(10, 2), S( 3,  -4)};
+  constexpr Score BlockedStorm[RANK_NB]  = {S( 0, 0), S( 0, 0), S( 76, 78), S(10,  0), S( 7,  0), S( 4, 0), S( 1, 0)};
   
   // Connected pawn bonus
   constexpr int Connected[RANK_NB] = { 0, 7, 8, 12, 29, 48, 86 };

c4ca433463f721baa74e70630b56d424d95a091d
Moez Jellouli <37274752+MJZ1977@users.noreply.github.com>
2020-05-23 22:20:36 +0200

    -6 blockedStorm

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -33,7 +33,7 @@ namespace {
 
   // Pawn penalties
   constexpr Score Backward        = S( 9, 24);
-  constexpr Score BlockedStorm    = S(80, 88);
+  constexpr Score BlockedStorm    = S(74, 82);
   constexpr Score Doubled         = S(11, 56);
   constexpr Score DoubledIsolated = S(15, 57);
   constexpr Score Isolated        = S( 5, 15);

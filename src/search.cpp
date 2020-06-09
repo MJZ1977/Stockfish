06acd22a7889ee970056f9d7fe2eab2cec7d0406
Moez Jellouli <37274752+MJZ1977@users.noreply.github.com>
2020-06-09 12:50:17 +0200

    depth / 2

--- src/search.cpp
+++ src/search.cpp
@@ -817,6 +817,7 @@ namespace {
 
         // Can ttValue be used as a better position evaluation?
         if (    ttValue != VALUE_NONE
+            && tte->depth() >= depth / 2
             && (tte->bound() & (ttValue > eval ? BOUND_LOWER : BOUND_UPPER)))
             eval = ttValue;
     }

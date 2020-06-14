54204303f25426266ed5265b4d67f5f722996429
Moez Jellouli <37274752+MJZ1977@users.noreply.github.com>
2020-06-14 16:54:10 +0200

    150

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -471,7 +471,7 @@ namespace {
                  +  69 * kingAttacksCount[Them]
                  +   3 * kingFlankAttack * kingFlankAttack / 8
                  +       mg_value(mobility[Them] - mobility[Us])
-                 + 100 * no_escape
+                 + 150 * no_escape
                  - 873 * !pos.count<QUEEN>(Them)
                  - 100 * bool(attackedBy[Us][KNIGHT] & attackedBy[Us][KING])
                  -   6 * mg_value(score) / 8

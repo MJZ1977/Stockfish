997ebceb27fa3348929592272e3f537fabf4a2fa
Moez Jellouli <37274752+MJZ1977@users.noreply.github.com>
2020-06-16 09:16:41 +0200

    Revert "static bonus"
    This reverts commit 8ce5dfec2503b85447c2d39f41980ab14fcd0a7f.

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -123,7 +123,7 @@ namespace {
 
   // PassedRank[Rank] contains a bonus according to the rank of a passed pawn
   constexpr Score PassedRank[RANK_NB] = {
-    S(0, 0), S(10, 28), S(17, 33), S(15, 41), S(64, 70), S(172, 173), S(282, 254)
+    S(0, 0), S(10, 28), S(17, 33), S(15, 41), S(62, 72), S(168, 177), S(276, 260)
   };
 
   // Assorted bonuses and penalties

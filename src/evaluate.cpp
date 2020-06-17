76e60abb226650af2d62dcb045dda5598da03f9f
Moez Jellouli <37274752+MJZ1977@users.noreply.github.com>
2020-06-17 09:46:45 +0200

    remove static

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -123,7 +123,7 @@ namespace {
 
   // PassedRank[Rank] contains a bonus according to the rank of a passed pawn
   constexpr Score PassedRank[RANK_NB] = {
-    S(0, 0), S(10, 28), S(21, 33), S(18, 44), S(64, 72), S(172, 177), S(280, 260)
+    S(0, 0), S(10, 28), S(17, 33), S(15, 41), S(62, 72), S(168, 177), S(276, 260)
   };
 
   // Assorted bonuses and penalties

a6c2b4ee0623aac6fae3a02dc7c10d64e7f6306f
Moez Jellouli <37274752+MJZ1977@users.noreply.github.com>
2020-06-27 20:47:20 +0200

    (mg-eg)/192

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -876,7 +876,7 @@ namespace {
     if (pos.non_pawn_material() > 8000 
         && abs(mg_value(score) - eg_value(score)) > Value(300)
         && abs(v) < Value(160))
-       v += Value(2 * (pos.this_thread()->nodes & 1) - 1) * int(mg_value(score) - eg_value(score)) / 64;
+       v += Value(2 * (pos.this_thread()->nodes & 1) - 1) * int(mg_value(score) - eg_value(score)) / 192;
 
     return v;
   }

4d572e20268fbbeee1a520d80f4558c8441b9ce0
MJZ1977 <37274752+MJZ1977@users.noreply.github.com>
2020-07-15 10:53:38 +0200

    1000x(1+d)

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -123,8 +123,8 @@ void MovePicker::score() {
                        + (*continuationHistory[0])[pos.moved_piece(m)][to_sq(m)]
                        - (1 << 28);
       }
-      if (depth < 7 && oppThreatMove && (to_sq(m) == from_sq(oppThreatMove) || from_sq(m) == to_sq(oppThreatMove)))
-          m.value += 16000 - 2000 * depth;
+      if (oppThreatMove && (to_sq(m) == from_sq(oppThreatMove) || from_sq(m) == to_sq(oppThreatMove)))
+          m.value += 1000 * (1 + depth);
    }
 }
 

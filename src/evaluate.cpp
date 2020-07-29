87e48594ac357b6c681bd3dccfdd763ce56168e4
Moez Jellouli <37274752+MJZ1977@users.noreply.github.com>
2020-07-29 11:30:43 +0200

    save

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -666,7 +666,7 @@ namespace {
                 // Otherwise assign a smaller bonus if the path to queen is not attacked
                 // and even smaller bonus if it is attacked but block square is not.
                 int k = !unsafeSquares                    ? 35 :
-                        !(unsafeSquares & squaresToQueen) ? 20 :
+                        !((unsafeSquares | pos.pieces(Them)) & squaresToQueen) ? 20 :
                         !(unsafeSquares & blockSq)        ?  9 :
                                                              0 ;
 

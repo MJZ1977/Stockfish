2e29e604f304e99b785b9a3c5e127311555605ea
Moez Jellouli <37274752+MJZ1977@users.noreply.github.com>
2020-08-12 21:07:00 +0200

    compensation (19,13)

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -180,7 +180,7 @@ namespace {
   constexpr Score CorneredBishop      = S( 50, 50);
   constexpr Score FlankAttacks        = S(  8,  0);
   constexpr Score Hanging             = S( 69, 36);
-  constexpr Score KnightOnQueen       = S( 16, 11);
+  constexpr Score KnightOnQueen       = S( 19, 13);
   constexpr Score LongDiagonalBishop  = S( 45,  0);
   constexpr Score MinorBehindPawn     = S( 18,  3);
   constexpr Score PassedFile          = S( 11,  8);

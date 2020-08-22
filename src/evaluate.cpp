a96c1c2599ecea09350d142f8530c21554e80412
Moez Jellouli <37274752+MJZ1977@users.noreply.github.com>
2020-08-22 10:11:21 +0200

    20-20-20-30

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -470,7 +470,7 @@ namespace {
     // Enemy rooks checks
     rookChecks = b1 & attackedBy[Them][ROOK] & safe;
     if (rookChecks)
-        kingDanger += SafeCheck[ROOK][more_than_one(rookChecks)];
+        kingDanger += SafeCheck[ROOK][more_than_one(rookChecks)] + bool(pos.castling_rights(Us)) * 20;
     else
         unsafeChecks |= b1 & attackedBy[Them][ROOK];
 
@@ -479,14 +479,14 @@ namespace {
     queenChecks =  (b1 | b2) & attackedBy[Them][QUEEN] & safe
                  & ~(attackedBy[Us][QUEEN] | rookChecks);
     if (queenChecks)
-        kingDanger += SafeCheck[QUEEN][more_than_one(queenChecks)];
+        kingDanger += SafeCheck[QUEEN][more_than_one(queenChecks)] + bool(pos.castling_rights(Us)) * 20;
 
     // Enemy bishops checks: count them only if they are from squares from which
     // opponent cannot give a queen check, because queen checks are more valuable.
     bishopChecks =  b2 & attackedBy[Them][BISHOP] & safe
                   & ~queenChecks;
     if (bishopChecks)
-        kingDanger += SafeCheck[BISHOP][more_than_one(bishopChecks)];
+        kingDanger += SafeCheck[BISHOP][more_than_one(bishopChecks)] + bool(pos.castling_rights(Us)) * 20;
 
     else
         unsafeChecks |= b2 & attackedBy[Them][BISHOP];
@@ -494,7 +494,7 @@ namespace {
     // Enemy knights checks
     knightChecks = attacks_bb<KNIGHT>(ksq) & attackedBy[Them][KNIGHT];
     if (knightChecks & safe)
-        kingDanger += SafeCheck[KNIGHT][more_than_one(knightChecks & safe)];
+        kingDanger += SafeCheck[KNIGHT][more_than_one(knightChecks & safe)] + bool(pos.castling_rights(Us)) * 30;
     else
         unsafeChecks |= knightChecks;
 

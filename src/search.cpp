0f9725c27e4bfcab51323f6f14e9cc1c4ccd6299
Moez Jellouli <37274752+MJZ1977@users.noreply.github.com>
2021-02-23 10:34:07 +0100

    compensation - 30
    bench 3724217

--- src/search.cpp
+++ src/search.cpp
@@ -1087,11 +1087,11 @@ moves_loop: // When in check, search starts from here
               // Futility pruning: parent node (~5 Elo)
               if (   lmrDepth < 7
                   && !ss->inCheck
-                  && ss->staticEval + 174 + 157 * lmrDepth <= alpha
+                  && ss->staticEval + 144 + 157 * lmrDepth <= alpha
                   &&  (*contHist[0])[movedPiece][to_sq(move)]
                     + (*contHist[1])[movedPiece][to_sq(move)]
                     + (*contHist[3])[movedPiece][to_sq(move)]
-                    + (*contHist[5])[movedPiece][to_sq(move)] / 3 < 25567 + 12 * int(alpha - ss->staticEval - 157 * lmrDepth))
+                    + (*contHist[5])[movedPiece][to_sq(move)] / 3 < 26463 + 8 * int(alpha - ss->staticEval - 157 * lmrDepth))
                   continue;
 
               // Prune moves with negative SEE (~20 Elo)
